437bf1767525f142d29de8ebadc442b964692d87
rui.liu <rui.liu@17zuoye.com>
2017-11-13 22:27:47 +0800

    测试@MessageHandler装饰器

--- test/webroot/mod1.js
+++ test/webroot/mod1.js
@@ -10,6 +10,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Injector_1 = require("../../dist/core/injector/Injector");
+const Core_1 = require("../../dist/core/Core");
 const Injector_2 = require("../../dist/engine/injector/Injector");
 const SystemUtil_1 = require("../../dist/utils/SystemUtil");
 const Engine_1 = require("../../dist/engine/Engine");
@@ -18,15 +19,26 @@ let Mod1 = class Mod1 {
         console.log("before sleep. " + new Date().toTimeString());
         await SystemUtil_1.sleep(2000);
         console.log(this._engine);
+        Core_1.core.dispatch("fuck", 1);
         console.log("after sleep. " + new Date().toTimeString());
         // 这里返回值
         ctx.body = "This is Mod1!!!";
+    }
+    async onFuck() {
+        await SystemUtil_1.sleep(2000);
+        console.log(arguments[0]);
     }
 };
 __decorate([
     Injector_1.Inject,
     __metadata("design:type", Engine_1.default)
 ], Mod1.prototype, "_engine", void 0);
+__decorate([
+    Injector_1.MessageHandler("fuck"),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", Promise)
+], Mod1.prototype, "onFuck", null);
 Mod1 = __decorate([
     Injector_2.ModuleClass
 ], Mod1);
--- test/webroot/mod1.ts
+++ test/webroot/mod1.ts
@@ -1,5 +1,6 @@
 import { Context } from "koa";
-import { Inject } from "../../dist/core/injector/Injector";
+import { Inject, MessageHandler } from "../../dist/core/injector/Injector";
+import { core } from "../../dist/core/Core";
 import { ModuleClass } from "../../dist/engine/injector/Injector";
 import { sleep } from "../../dist/utils/SystemUtil";
 import IModule from "../../dist/engine/module/IModule";
@@ -16,8 +17,16 @@ export default class Mod1 implements IModule
         console.log("before sleep. " + new Date().toTimeString());
         await sleep(2000);
         console.log(this._engine);
+        core.dispatch("fuck", 1);
         console.log("after sleep. " + new Date().toTimeString());
         // 这里返回值
         ctx.body = "This is Mod1!!!";
+    }
+
+    @MessageHandler("fuck")
+    private async onFuck():Promise<void>
+    {
+        await sleep(2000);
+        console.log(arguments[0]);
     }
 }
